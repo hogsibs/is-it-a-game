d9e67c1c4a166efe451d26e4795fcd09dffe73fc
Tyler Hogsett <tyhogset@microsoft.com>
2020-11-15 12:41:30 -0800

    if this is a game, its loop just got more sophisticated

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
-async function runGameLoop()
+function startGameLoop()
 {
-    const minimumFrameInterval = 1;
-    let time = (new Date()).getTime();
-    while(true)
-    {
-        let newTime = new Date().getTime();
-        let timePassed = newTime - time;
-        if(timePassed >= minimumFrameInterval)
-        {
-            await draw(newTime, timePassed);
-            time = newTime;
-        }
-        await sleep();
-    }
+    const currentTime = performance.now();
+    const gameState = initializeGameState();
+    runUpdateLoop(gameState);
+    runDrawLoop(currentTime, gameState);
 }
 
-async function draw(time, timePassed)
+function initializeGameState()
+{
+    return {
+        time: performance.now(),
+    };
+}
+
+function runUpdateLoop(gameState)
+{
+    update(gameState);
+    setImmediate(() => runUpdateLoop(gameState));
+}
+
+function update(gameState)
+{
+    gameState.time = performance.now();
+}
+
+function runDrawLoop(time, gameState)
+{
+    const newTime = performance.now();
+    const delta = newTime - time;
+    draw(delta, gameState);
+    window.requestAnimationFrame(() => runDrawLoop(newTime, gameState));
+}
+
+function draw(delta, gameState)
 {
     document.body.innerHTML = `
-        <div>Current Time: ${time}</div>
-        <div>Time Passed: ${timePassed}</div>
-        <div>Framerate: ${Math.round(1000 / timePassed)}</div>
+        <div>Current Time: ${gameState.time}</div>
+        <div>Time Passed: ${delta}</div>
+        <div>Framerate: ${Math.round(1000 / delta)}</div>
     `;
-}
-
-function sleep()
-{
-    return new Promise((resolve) => setImmediate(resolve));
 }
