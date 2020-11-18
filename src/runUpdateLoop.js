63a1fb518b809d8e4c48e3651383f87cdbff2a05
Tyler Hogsett <tyhogset@microsoft.com>
2020-11-18 14:09:38 -0800

    cleaned up movement

--- src/runUpdateLoop.js
+++ src/runUpdateLoop.js
@@ -14,44 +14,54 @@ function update(delta, gameState)
 {
     gameState.time = performance.now();
 
-    let directionDelta = delta;
-    if(gameState.player.direction === gameState.player.targetDirection)
+    const velocityX = gameState.player.velocity.magnitude *
+        Math.cos(gameState.player.velocity.direction);
+    const velocityY = gameState.player.velocity.magnitude *
+        Math.sin(gameState.player.velocity.direction);
+    const accelerationX = gameState.player.acceleration.magnitude *
+        Math.cos(gameState.player.acceleration.direction) *
+        (delta / 1000);
+    const accelerationY = gameState.player.acceleration.magnitude *
+        Math.sin(gameState.player.acceleration.direction) *
+        (delta / 1000);
+    const frictionX = -1 * velocityX * gameState.player.friction * (delta / 1000);
+    const frictionY = -1 * velocityY * gameState.player.friction * (delta / 1000);
+    const newVelocityX = velocityX + accelerationX + frictionX;
+    const newVelocityY = velocityY + accelerationY + frictionY;
+    gameState.player.velocity.magnitude =
+        Math.sqrt(
+            Math.pow(newVelocityX, 2) +
+            Math.pow(newVelocityY, 2));
+    if(newVelocityX === 0)
     {
-        if(gameState.player.timeLeftInCurrentDirection > delta)
-        {
-            gameState.player.timeLeftInCurrentDirection -= delta;
-            directionDelta = 0;
+        if(newVelocityY > 0) {
+            gameState.player.velocity.direction = Math.PI / 2;
         } else {
-            gameState.player.targetDirection =
-                gameState.player.direction +
-                (   (Math.random() > 0.5 ? 1 : -1) *
-                    (Math.random() * Math.PI / 2 + Math.PI / 2));
-            directionDelta = delta - gameState.player.timeLeftInCurrentDirection;
+            gameState.player.velocity.direction = Math.PI / -2;
         }
-    }
-
-    if(gameState.player.direction !== gameState.player.targetDirection)
-    {
-        const diff = gameState.player.targetDirection - gameState.player.direction;
-        if(Math.abs(diff) < (directionDelta / 1000) * gameState.player.turnSpeed)
-        {
-            gameState.player.direction = gameState.player.targetDirection;
-            gameState.player.timeLeftInCurrentDirection = Math.round(Math.random() * 2000) + 1000;
-        } else {
-            const radialDirection = diff / Math.abs(diff);
-            gameState.player.direction +=
-                radialDirection *
-                gameState.player.turnSpeed *
-                (directionDelta / 1000);
-        }
+    } else {
+        gameState.player.velocity.direction = Math.atan2(newVelocityY, newVelocityX);
     }
     
     gameState.player.position.x +=
-        Math.cos(gameState.player.direction) *
+        Math.cos(gameState.player.velocity.direction) *
         (delta / 1000) *
-        gameState.player.speed;
+        gameState.player.velocity.magnitude;
     gameState.player.position.y +=
-        Math.sin(gameState.player.direction) *
+        Math.sin(gameState.player.velocity.direction) *
         (delta / 1000) *
-        gameState.player.speed;
+        gameState.player.velocity.magnitude;
+    
+    gameState.player.timeLeftInCurrentDirection -= delta;
+    if(gameState.player.timeLeftInCurrentDirection <= 0)
+    {
+        gameState.player.timeLeftInCurrentDirection = Math.random() * 2000 + 1000;
+        if(Math.random() < 0.2)
+        {
+            gameState.player.acceleration.magnitude = 0;
+        } else {
+            gameState.player.acceleration.magnitude = gameState.player.power;
+            gameState.player.acceleration.direction = Math.random() * 2 * Math.PI;
+        }
+    }
 }
