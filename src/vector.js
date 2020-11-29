6d4ca313a95a2d4f9b47f79b68bc968da0cc1b7f
Joshua Hogsett <jthogsett@gmail.com>
2020-11-28 16:23:11 -0800

    break vector down to its components

--- src/vector.js
+++ src/vector.js
@@ -17,44 +17,75 @@ export function setMagnitude(vector, magnitude) {
 }
 
 class LazyVector {
+    constructor() {
+        this.x = new Lazy(() => this.calculateX());
+        this.y = new Lazy(() => this.calculateY());
+        this.angle = new Lazy(() => this.calculateAngle());
+        this.magnitude = new Lazy(() => this.calculateMagnitude());
+    }
+    calculateX() {
+        return Math.cos(this.angle.getValue()) * this.magnitude.getValue();
+    }
+    calculateY() {
+        return Math.sin(this.angle.getValue()) * this.magnitude.getValue();
+    }
+    calculateAngle() {
+        return Math.atan2(this.y.getValue(), this.x.getValue());
+    }
+    calculateMagnitude() {
+        return Math.sqrt(Math.pow(this.x.getValue(), 2) + Math.pow(this.y.getValue(), 2));
+    }
     setCartesian({ x, y }) {
-        this.cartesian = { x, y };
-        delete this.getCartesian;
-        this.getEuclidean = () => this.lazyGetEuclidean();
+        this.x.setValue(x);
+        this.y.setValue(y);
+        this.angle.reset();
+        this.magnitude.reset();
     }
     getCartesian() {
-        return { ...this.cartesian };
+        return {
+            x: this.getX(),
+            y: this.getY()
+        };
     }
-    lazyGetCartesian() {
-        this.cartesian = asCartesian(this.euclidean);
-        delete this.getCartesian;
-        return this.getCartesian();
+    getX() {
+        return this.x.getValue();
+    }
+    getY() {
+        return this.y.getValue();
     }
     setEuclidean({ angle, magnitude }) {
-        this.euclidean = { angle, magnitude };
-        delete this.getEuclidean;
-        this.getCartesian = () => this.lazyGetCartesian();
+        this.angle.setValue(angle);
+        this.magnitude.setValue(magnitude);
+        this.x.reset();
+        this.y.reset();
     }
     getEuclidean() {
-        return { ...this.euclidean };
-    }
-    lazyGetEuclidean() {
-        this.euclidean = asEuclidean(this.cartesian);
-        delete this.getEuclidean;
-        return this.getEuclidean();
+        return {
+            angle: this.angle.getValue(),
+            magnitude: this.magnitude.getValue()
+        };
     }
 }
 
-function asCartesian({ angle, magnitude }) {
-    return {
-        x: magnitude * Math.cos(angle),
-        y: magnitude * Math.sin(angle)
-    };
-}
-
-function asEuclidean({ x, y }) {
-    return {
-        angle: Math.atan2(y, x),
-        magnitude: Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
-    };
+class Lazy {
+    constructor(getter) {
+        this.getter = getter;
+        this.getValue = this.lazyGetValue;
+    }
+    getValue() {
+        return this.value;
+    }
+    lazyGetValue() {
+        this.value = this.getter();
+        delete this.getValue;
+        return this.value;
+    }
+    reset() {
+        delete this.value;
+        this.getValue = this.lazyGetValue;
+    }
+    setValue(value) {
+        this.value = value;
+        delete this.getValue;
+    }
 }
